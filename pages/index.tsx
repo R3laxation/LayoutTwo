5c1180bf501594bc3f15e304f23d8f4cc3e8e6e4
R3laxation <memories@bk.ru>
2022-02-22 21:06:06 +0600

    Set main font for project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
       <div>
-      123132132131
+            <h1>Мой супер заголовок</h1>
+            <h2>Мой супер заголовок</h2>
+            <h3>Мой супер заголовок</h3>
+            <h4>Мой супер заголовок</h4>
       </div>
 
   )
