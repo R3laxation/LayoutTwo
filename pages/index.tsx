12f603994e9c1f0ab39c35d7bdda497f41c2657b
R3laxation <memories@bk.ru>
2022-11-21 11:28:35 +0600

    Created header skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
 import styles from '../styles/home.module.scss';
+import cl from 'classnames';
 
 export default function Home() {
   return (
       <div>
-            <h1>Мой супер заголовок</h1>
-            <h2>Мой супер заголовок</h2>
-            <h3>Мой супер заголовок</h3>
-            <h4>Мой супер заголовок</h4>
+        <section className={cl(styles.section, styles.sectionMain)}>
+            <header>
+                <Logo/>
+                <SocialNetworks/>
+                <Button onClick={() => {}}>Whitepaper</Button>
+            </header>
+           <Navigation/>
+        </section>
+          <div>
+              <Title type={'h2'}>From dust to dawn</Title>
+              <ul>
+                  <li><Button onClick={() => {}}>Mint</Button></li>
+                  <li><Button onClick={() => {}}>Connect Wallet</Button></li>
+
+              </ul>
+          </div>
       </div>
 
   )
