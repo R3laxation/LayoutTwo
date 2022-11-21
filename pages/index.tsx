42328fa0f80fd286d3c8f0ee9ead731c8ddc31ec
R3laxation <memories@bk.ru>
2022-11-21 15:07:02 +0600

    Changed styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,19 @@ export default function Home() {
         <section className={cl(styles.section, styles.sectionMain)}>
             <header>
                 <Logo/>
-                <SocialNetworks/>
-                <Button onClick={() => {}}>Whitepaper</Button>
+                {/*<SocialNetworks/>*/}
+                {/*<Button onClick={() => {}}>Whitepaper</Button>*/}
             </header>
-           <Navigation/>
+        {/*   <Navigation/>*/}
         </section>
-          <div>
-              <Title type={'h2'}>From dust to dawn</Title>
-              <ul>
-                  <li><Button onClick={() => {}}>Mint</Button></li>
-                  <li><Button onClick={() => {}}>Connect Wallet</Button></li>
+        {/*  <div>*/}
+        {/*      <Title type={'h2'}>From dust to dawn</Title>*/}
+        {/*      <ul>*/}
+        {/*          <li><Button onClick={() => {}}>Mint</Button></li>*/}
+        {/*          <li><Button onClick={() => {}}>Connect Wallet</Button></li>*/}
 
-              </ul>
-          </div>
+        {/*      </ul>*/}
+        {/*  </div>*/}
       </div>
 
   )
