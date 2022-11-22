a5b2a586821789923f8d69bbcbe25b3f39387868
R3laxation <memories@bk.ru>
2022-11-22 10:25:07 +0600

    Filled Navigation component, add React.FC

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -6,9 +6,7 @@ interface NavigationProps {}
 export const Navigation: React.FC<NavigationProps> = ({}) => {
     return (
         <div>
-            <ul>
-                <li></li>
-            </ul>
+            Navigation
         </div>
     );
 };
