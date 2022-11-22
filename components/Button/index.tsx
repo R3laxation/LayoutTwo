868a708095b6263812212be770cbddcc0219cace
R3laxation <memories@bk.ru>
2022-11-22 10:01:36 +0600

    Created Button component skeleton

---
+++ components/Button/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './style.module.scss';
+
+interface ButtonProps {}
+
+export const Button = ({}) => {
+    return (
+        <div>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    );
+};
+
