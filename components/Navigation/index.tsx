d0e88a7ba4b413ebff552b8e2e0ca16ae1220530
R3laxation <memories@bk.ru>
2022-11-23 21:42:12 +0600

    Changed Navigation component

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -1,13 +1,33 @@
+import Link from 'next/link';
 import React from 'react';
 import styles from './style.module.scss';
 
-interface NavigationProps {}
+interface NavigationProps {
+}
+
+const links = [
+    {id: 1, title: 'Home', href: '/'},
+    {id: 2, title: 'Factions', href: '/'},
+    {id: 3, title: 'AntDesign', href: '/'},
+];
+
 
 export const Navigation: React.FC<NavigationProps> = ({}) => {
     return (
-        <div>
-            Navigation
-        </div>
+        <nav>
+            <ul>
+                <li>
+                    {links.map((link) => (
+                        <Link
+                            href={link.href}
+                            key={link.id}
+                        >
+                            {link.title}
+                        </Link>
+                    ))}
+                </li>
+            </ul>
+        </nav>
     );
 };
 
